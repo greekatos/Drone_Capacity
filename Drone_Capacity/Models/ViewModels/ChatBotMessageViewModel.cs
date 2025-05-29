dbde33a2aa69075cf1beaed2b99195fd8256fcda
Greekatos <dimmin99@gmail.com>
2025-05-29 19:02:27 +0300

    End of third use case

---
+++ Drone_Capacity/Models/ViewModels/ChatBotMessageViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using CommunityToolkit.Maui.Views;
+using Drone_Capacity.Views;
+using Microsoft.Maui.Controls;
+
+namespace Drone_Capacity.Models.ViewModels
+{
+    public class ChatBotMessageViewModel
+    {
+        // Command for navigation of MyFields page to Home Button (the back button)
+        public ICommand NavigateBackToMainChatBotPage { get; }
+
+        public ChatBotMessageViewModel()
+        {
+            NavigateBackToMainChatBotPage = new Command(async () => await NavigateBack());
+        }
+
+        private async Task NavigateBack()
+        {
+            await Shell.Current.GoToAsync("//ChatBotPage");
+        }
+    }
+}
--- Drone_Capacity/Views/ChatBotMessengerPage.xaml.cs
+++ Drone_Capacity/Views/ChatBotMessengerPage.xaml.cs
@@ -1,9 +1,13 @@
 namespace Drone_Capacity.Views;
+using Drone_Capacity.Models.ViewModels;
+using Microsoft.Maui.Controls;
+
 
 public partial class ChatBotMessengerPage : ContentPage
 {
 	public ChatBotMessengerPage()
 	{
 		InitializeComponent();
+		BindingContext = new ChatBotMessageViewModel();
 	}
 }
